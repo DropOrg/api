5b3bbb5a2f330c1b271a60573ac6e004dd8aa954
Shan Tulshi <shan.tulshi@gmail.com>
2018-11-25 17:06:14 -0600

    Model permissions exports

--- api/models/index.ts
+++ api/models/index.ts
@@ -1,3 +1,4 @@
 export { IUser, UserSchema, User } from './users';
 export { IDocklet, IProject, ProjectSchema, Project } from './projects';
 export { IImage } from './image';
+export { Access, AuthStrategy } from './permissions';
