f6c9b462b7dd4fa29c5464c34c0072e6aeb692d6
Shan Tulshi <shan.tulshi@gmail.com>
2018-11-25 17:05:37 -0600

    Image exports

--- api/models/index.ts
+++ api/models/index.ts
@@ -1,2 +1,3 @@
-export { IUser, UserSchema, User } from './users'
-export { IDocklet, IProject, ProjectSchema, Project } from './projects'
+export { IUser, UserSchema, User } from './users';
+export { IDocklet, IProject, ProjectSchema, Project } from './projects';
+export { IImage } from './image';
